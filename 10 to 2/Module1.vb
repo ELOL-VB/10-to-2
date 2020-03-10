c3122022ac1f0a9728967939fd8bdf86c99abc62
ZEZE <55626553+EZ-Super@users.noreply.github.com>
2020-03-10 21:38:45 +0800

    Add files via upload

---
+++ 10 to 2/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim a, b, c, d As Integer
+        Dim e As String
+        a = 0 : b = 0 : c = 0 : d = 0 : e = ""
+        Console.WriteLine("輸入數字")
+        a = Console.ReadLine()
+        Do Until c = 1
+            b = a Mod 2
+            c = a \ 2
+            a = c
+            e = b & e
+            d = c & e
+
+        Loop
+        Console.WriteLine(d)
+        Console.Read()
+    End Sub
+
+End Module
